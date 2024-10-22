3dd76f5f75886a7561bc8ff3105ae8a974c5aa2a
Dzhalilov Amin <amin.tj2010@gmail.com>
2024-10-22 17:03:45 +0500

    creating about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "../About/style.css";
+import MiniCard from '../../components/miniCard/miniCard';"../../components/miniCard/miniCard";
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="sect1">
+        <div className="s1-text">
+          <img src="src/icons/AboutUs.png" alt="" />
+          <h1>About Us</h1>
+          <p>Our agency has been successfully operating in digital marketing since 2012. We are proud to offer you quality and exemplary service. We help our clients get better results with our creative and data-driven approach to online advertising. </p>
+        </div>
+        <div className='flex '>
+          <div>
+              <img src="" alt="" />
+          </div>
+        </div>
+    </div>
   )
 }
 
